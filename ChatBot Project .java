2fa6a179d6d9de3c50ba0452cac37882d732f558
ALphaTrion21 <144452060+ALphaTrion21@users.noreply.github.com>
2023-10-02 00:55:50 +0530

    Day(0);
    chatbot progress 0;

---
+++ ChatBot Project .java
@@ -0,0 +1,29 @@
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+
+public class ChatGPTClient {
+    public static void main(String[] LOL) {
+        try {
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpPost request = new HttpPost("https://api.openai.com/v1/engines/davinci/completions"); 
+
+            
+            request.addHeader("Authorization", "Bearer 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");
+
+           // input prompt
+            StringEntity entity = new StringEntity("{\"prompt\": \"Translate the following English text to French: 'Hello, how are you?'\"}");
+            request.setEntity(entity);
+
+            
+            HttpResponse response = httpClient.execute(request);
+
+
+            System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
